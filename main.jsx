43701c3e4fd86da2e8d5cbd41680aee0f547f49b
unjiayi1-glitch <unjiayi1@gmail.com>
2026-05-25 01:36:38 +0800

    src/

--- main.jsx
+++ main.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-const rootElement = document.getElementById('root')
-
-if (!rootElement) {
-  throw new Error('Root element with id "root" not found in HTML')
-}
-
-const root = ReactDOM.createRoot(rootElement)
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
